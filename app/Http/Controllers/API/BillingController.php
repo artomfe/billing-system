0ae2f38235bfc8db11c5d3e9d2c08772c82fe02f
Artom <artomfj@gmail.com>
2024-07-08 18:40:32 -0300

    fix: corrigi as validações de request da api

--- app/Http/Controllers/API/BillingController.php
+++ app/Http/Controllers/API/BillingController.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Requests\ProcessFileRequest;
 use App\Services\BillingService;
 use App\Models\Billing;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ProcessFileRequest;
 
 /**
     * @OA\Info(
--- app/Http/Requests/ProcessFileRequest.php
+++ app/Http/Requests/ProcessFileRequest.php
@@ -8,21 +8,39 @@ class ProcessFileRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
+     *
+     * @return bool
      */
-    public function authorize(): bool
+    public function authorize()
     {
-        return false;
+        return true; 
     }
 
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     * @return array
      */
-    public function rules(): array
+    public function rules()
     {
         return [
-            //
+            'input_file' => 'required|file|mimes:csv,txt|max:102400', 
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'input_file.required' => 'O arquivo é obrigatório.',
+            'input_file.file' => 'O arquivo deve ser um arquivo válido.',
+            'input_file.mimes' => 'O arquivo deve ser um arquivo do tipo: csv, txt.',
+            'input_file.max' => 'O arquivo não deve exceder 100MB.',
         ];
     }
 }
+
